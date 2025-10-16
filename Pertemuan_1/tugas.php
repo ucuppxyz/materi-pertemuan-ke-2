bb2b44e77d68cd3c72c3dc161f407f583f951bb4
ucuppxyz <ucupptegal@gmail.com>
2025-10-16 19:31:54 +0700

    TUGAS-1

---
+++ Pertemuan_1/tugas.php
@@ -0,0 +1,21 @@
+<?php
+
+// $tanggal = 05;
+// $angka = $tanggal;
+// while ($angka < 100 ){
+//     echo $angka . " ";
+//     $angka += $tanggal;
+// }
+
+
+// $nama = "yusuf";
+// $jumlahhuruf = strlen($nama);
+// $i = 1;
+// do {
+//     echo "belajar php \n";
+//     $i++;
+
+// } while ($i <= $jumlahhuruf);
+
+
+?>
